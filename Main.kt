4fa34d000679043378523a5cd15d107afe076f4d
Khabi <khabimfw@gmail.com>
2022-02-22 18:52:39 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,5 +1,20 @@
+fun division(){
+    println("-----------------------------------------------------------------------------------------------------")
+}
 
 fun main(args: Array<String>) {
+
+    division()
+
+//  Binary Search
     val bs = BinarySearch()
-    print(bs.binarySearch(99))
+    println("Position in array: " + bs.binarySearch(99))
+
+    division()
+
+//  Selection Sort Array
+    val sa = SelectionSort()
+    println("Sorted array: " + sa.selectionSort())
+
+    division()
 }
